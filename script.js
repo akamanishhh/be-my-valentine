e66320bc9d38857685022f82f8b7ee0edbd0cf47
root <root@Milan-Adex>
2024-02-07 22:51:55 +0545

    changes

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const yesButton = document.querySelector(".btn--yes");
 const noButton = document.querySelector(".btn--no");
 const catImg = document.querySelector(".cat-img");
 
-const MAX_IMAGES = 4;
+const MAX_IMAGES = 3;
 
 let play = true;
 let noCount = 0;
